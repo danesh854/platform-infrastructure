845383bb20935a6705a065164283d13902eada52
danesh kabade <daneshvkaade@gmail.com>
2026-06-04 20:56:42 +0530

    Create production VPC networking module

---
+++ environments/dev/main.tf
@@ -0,0 +1,40 @@
+module "networking" {
+
+
+  source = "../../modules/networking"
+
+
+  project_name = "enterprise-dev"
+
+
+  vpc_cidr = "10.0.0.0/16"
+
+
+  public_subnets = [
+
+    "10.0.1.0/24",
+
+    "10.0.2.0/24"
+
+  ]
+
+
+  private_subnets = [
+
+    "10.0.10.0/24",
+
+    "10.0.20.0/24"
+
+  ]
+
+
+  availability_zones = [
+
+    "ap-south-1a",
+
+    "ap-south-1b"
+
+  ]
+
+
+}
